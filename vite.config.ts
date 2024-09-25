a958267fa964efff3c62daaed439c27aa6a8140e
hoi <hoi@servers.com>
2024-09-25 19:46:33 +0300

    installed linaria, postCss custom media

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import linaria from "@linaria/vite";
+import postcssGlobalData from '@csstools/postcss-global-data';
+import postcssCustomMedia from 'postcss-custom-media';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        linaria({
+            include: ["**/*.{ts,tsx}"],
+            babelOptions: {
+                presets: ["@babel/preset-typescript", "@babel/preset-react"],
+            },
+            sourceMap: process.env.NODE_ENV !== 'production',
+        }),
+    ],
+    css: {
+        postcss: {
+            plugins: [
+                postcssGlobalData({
+                    files: ['./src/index.css'],
+                }),
+                postcssCustomMedia(),
+            ],
+        },
+    },
+
 })
