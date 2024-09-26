c2d7749bc5a072818e13df6ad2dbb46f1dca5ca8
hoi <hoi@servers.com>
2024-09-26 11:19:41 +0300

    linaria removed, styled-components installed instead

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,9 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
-import linaria from "@linaria/vite";
-import postcssGlobalData from '@csstools/postcss-global-data';
-import postcssCustomMedia from 'postcss-custom-media';
+// vite.config.ts
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
-        react(),
-        linaria({
-            include: ["**/*.{ts,tsx}"],
-            babelOptions: {
-                presets: ["@babel/preset-typescript", "@babel/preset-react"],
-            },
-            sourceMap: process.env.NODE_ENV !== 'production',
-        }),
+        react()
     ],
-    css: {
-        postcss: {
-            plugins: [
-                postcssGlobalData({
-                    files: ['./src/index.css'],
-                }),
-                postcssCustomMedia(),
-            ],
-        },
-    },
-
-})
+});
