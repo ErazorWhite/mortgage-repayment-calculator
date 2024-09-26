8daf3c3a0298d7822f7b2a1d1c1892e568e2af1e
hoi <hoi@servers.com>
2024-09-26 12:30:28 +0300

    fonts presets created in typography

---
+++ src/global/theme.ts
@@ -0,0 +1,37 @@
+export const typography = {
+    preset1: {
+        fontSize: '56px',
+        lineHeight: '125%',
+        letterSpacing: '-1px',
+        fontFamily: 'Plus Jakarta Sans Bold, sans-serif',
+        fontWeight: 'bold',
+    },
+    preset2: {
+        fontSize: '24px',
+        lineHeight: '125%',
+        letterSpacing: '-1px',
+        fontFamily: 'Plus Jakarta Sans Bold, sans-serif',
+        fontWeight: 'bold',
+    },
+    preset3: {
+        fontSize: '18px',
+        lineHeight: '125%',
+        letterSpacing: '-1px',
+        fontFamily: 'Plus Jakarta Sans Bold, sans-serif',
+        fontWeight: 'bold',
+    },
+    preset4: {
+        fontSize: '16px',
+        lineHeight: '150%',
+        letterSpacing: '0px',
+        fontFamily: 'Plus Jakarta Sans Medium, sans-serif',
+        fontWeight: 'medium',
+    },
+    preset5: {
+        fontSize: '14px',
+        lineHeight: '150%',
+        letterSpacing: '0px',
+        fontFamily: 'Plus Jakarta Sans Medium, sans-serif',
+        fontWeight: 'medium',
+    },
+};
