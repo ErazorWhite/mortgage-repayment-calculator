3bc884c8fffe205c1c4983263319e4805b6b00c8
hoi <hoi@servers.com>
2024-09-26 16:51:00 +0300

    Added styles

---
+++ src/components/RadioGroup/RadioGroup.styles.ts
@@ -0,0 +1,78 @@
+import styled from "styled-components";
+import {typography} from "../../global/theme";
+
+export const RadioLegend = styled.legend`
+    ${typography.preset4};
+    padding-bottom: 12px;
+
+    color: var(--color-state-700);
+`
+
+export const Li = styled.li`
+    padding-bottom: 12px;
+`
+export const CheckMark = styled.span`
+    position: relative;
+    display: inline-block;
+    margin-right: 16px;
+    width: 20px;
+    height: 20px;
+    border: 2px solid var(--color-state-700);
+    border-radius: 50%;
+    background-color: transparent;
+    transition: background-color var(--animation);
+`
+export const RadioInput = styled.input`
+    position: absolute;
+    opacity: 0;
+    cursor: pointer;
+    height: 0;
+    width: 0;
+    
+    &:focus + ${CheckMark} {
+        outline: 2px solid black;
+    }
+    
+    &:checked + ${CheckMark} {
+        background-color: var(--color-lime-opacity);
+        border-color: var(--color-lime);
+        
+        &::after {
+            content: '';
+            display: block;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            width: 10px;
+            height: 10px;
+            background-color: var(--color-lime); /* Цвет внутреннего кружка */
+            border-radius: 50%;
+            transform: translate(-50%, -50%);
+        }
+    }
+
+`
+
+export const RadioLabel = styled.label`
+    ${typography.preset3};
+    position: relative;
+    display: flex;
+    align-items: center;
+    padding: 13px 16px;
+    border: 1px solid var(--color-state-700);
+    border-radius: 4px;
+    color: var(--color-state-900);
+    cursor: pointer;
+    transition: var(--animation);
+    
+    &:hover {
+        border-color: var(--color-lime);
+    }
+
+    &:has(${RadioInput}:checked) {
+        background-color: var(--color-lime-opacity);
+        border-color: var(--color-state-500);
+    }
+`
+
+
--- src/components/RadioGroup/RadioGroup.tsx
+++ src/components/RadioGroup/RadioGroup.tsx
@@ -1,20 +1,29 @@
 import {RadioGroupProps} from "../../global/types.ts";
+import {CheckMark, Li, RadioInput, RadioLabel, RadioLegend} from "./RadioGroup.styles";
 
-export const RadioGroup = ({label, name, options, register, error}: RadioGroupProps) => (
-    <div>
-        <legend>{label}</legend>
-        {options.map((option) => (
-            <label key={option.value} htmlFor={option.value}>
-                <input
-                    {...register(name)}
-                    type="radio"
-                    id={option.value}
-                    value={option.value}
-                />
-                {option.label}
-            </label>
-        ))}
-        {error && <p>{error.message}</p>}
+export const RadioGroup = ({label, name, options, register, error}: RadioGroupProps) => {
 
-    </div>
-);
+    return (
+        <div>
+            <RadioLegend>{label}</RadioLegend>
+            <ul>
+                {options.map((option) => (
+                    <Li>
+                        <RadioLabel key={option.value} htmlFor={option.value}>
+                            <RadioInput
+                                {...register(name)}
+                                type="radio"
+                                id={option.value}
+                                value={option.value}
+                            />
+                            <CheckMark/>
+                            {option.label}
+                        </RadioLabel>
+                    </Li>
+                ))}
+            </ul>
+            {error && <p>{error.message}</p>}
+
+        </div>
+    )
+};
