67c7d7e0c7d862ab216ade237c2864b036aa960c
hoi <hoi@servers.com>
2024-10-24 10:42:45 +0300

    change result calculation round logic

--- src/components/form/CalculatorForm/CalculatorForm.tsx
+++ src/components/form/CalculatorForm/CalculatorForm.tsx
@@ -1,15 +1,15 @@
-import {SubmitErrorHandler, SubmitHandler, useForm} from "react-hook-form";
-import {useCallback, useState} from "react";
+import {SubmitHandler, useForm} from 'react-hook-form';
+import {useCallback, useState} from 'react';
 import {yupResolver} from '@hookform/resolvers/yup';
-import {MortgageData, MortgageType} from "./types.ts";
-import {RadioGroup} from "../RadioGroup/RadioGroup.tsx";
-import {mortgageCalc} from "../../../utilities/mortgageCalc.ts";
-import {ClearButton, Form, FormHeader, FormThumb, FullInputSeparator, H1, HalfInputSeparator, InputsContainer} from "./CalculatorForm.styled";
-import {CalculateButton} from "../../CalculateButton/CalculateButton";
-import {MortgageDataSchema} from "../../../utilities/validationSchemas";
-import {FormattedNumberInput} from "../FormattedNumberInput/FormattedNumberInput.tsx";
-import {CalculationResult} from "../../Results/CalculationResult/CalculationResult.tsx";
-import {EmptyResult} from "../../Results/EmptyResult/EmptyResult.tsx";
+import {MortgageData, MortgageType} from './types.ts';
+import {RadioGroup} from '../RadioGroup/RadioGroup.tsx';
+import {calculateMortgage} from '../../../utilities/calculateMortgage.ts';
+import {ClearButton, Form, FormHeader, FormThumb, FullInputSeparator, H1, HalfInputSeparator, InputsContainer} from './CalculatorForm.styled';
+import {CalculateButton} from '../../CalculateButton/CalculateButton';
+import {MortgageDataSchema} from '../../../utilities/validationSchemas';
+import {FormattedNumberInput} from '../FormattedNumberInput/FormattedNumberInput.tsx';
+import {CalculationResult} from '../../Results/CalculationResult/CalculationResult.tsx';
+import {EmptyResult} from '../../Results/EmptyResult/EmptyResult.tsx';
 
 const radioOptions = [
     {label: MortgageType.Repayment, value: MortgageType.Repayment},
@@ -25,14 +25,14 @@ export const CalculatorForm = () => {
         handleSubmit,
         reset,
         formState: {errors}
-    } = useForm<MortgageData>({resolver: yupResolver(MortgageDataSchema), mode: "onTouched"});
+    } = useForm<MortgageData>({resolver: yupResolver(MortgageDataSchema), mode: 'onTouched'});
 
     const onSubmit: SubmitHandler<MortgageData> = useCallback(({amount, term, rate, type}) => {
 
-        const {monthlyPayment, totalRepayment} = mortgageCalc({term, rate, type, amount});
+        const {monthlyPayment, totalRepayment} = calculateMortgage({term, rate, type, amount});
 
-        setMonthlyRepayment(parseFloat(monthlyPayment.toFixed(2)));
-        setTotalRepayment(parseFloat(totalRepayment.toFixed(2)));
+        setMonthlyRepayment(monthlyPayment);
+        setTotalRepayment(totalRepayment);
 
     }, [])
 
@@ -47,28 +47,28 @@ export const CalculatorForm = () => {
             <Form onSubmit={handleSubmit(onSubmit)}>
                 <FormHeader>
                     <H1>Mortgage Calculator</H1>
-                    <ClearButton type="button" onClick={handleClear}>Clear All
+                    <ClearButton type='button' onClick={handleClear}>Clear All
                     </ClearButton>
                 </FormHeader>
                 <InputsContainer>
                     <FullInputSeparator>
-                        <FormattedNumberInput name="amount" label="Mortgage Amount" prefix="£" control={control}/>
+                        <FormattedNumberInput name='amount' label='Mortgage Amount' prefix='£' control={control}/>
                     </FullInputSeparator>
                     <HalfInputSeparator>
-                        <FormattedNumberInput name="term" label="Mortgage Term" suffix="years" control={control}/>
+                        <FormattedNumberInput name='term' label='Mortgage Term' suffix='years' control={control}/>
                     </HalfInputSeparator>
                     <HalfInputSeparator>
-                        <FormattedNumberInput name="rate" label="Interest rate" suffix="%" control={control}/>
+                        <FormattedNumberInput name='rate' label='Interest rate' suffix='%' control={control}/>
                     </HalfInputSeparator>
                 </InputsContainer>
                 <RadioGroup
-                    label="Mortgage Type"
-                    name="type"
+                    label='Mortgage Type'
+                    name='type'
                     options={radioOptions}
                     register={register}
                     error={errors.type}
                 />
-                <CalculateButton type="submit">Calculate Repayments</CalculateButton>
+                <CalculateButton type='submit'>Calculate Repayments</CalculateButton>
             </Form>
             {
                 monthlyRepayment && totalRepayment ?
